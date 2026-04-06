e1edd608c59ab9601cd86e4168547bb73bc86f44
Huy <quochuybt2005@gmail.com>
2026-04-06 14:28:56 +0700

    bai3 tuan06

--- Buoi06_23664121/src/App.jsx
+++ Buoi06_23664121/src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.css";
 import Bai1 from "./components/Bai1";
 import Bai2 from "./components/Bai2";
+import Bai3 from "./components/Bai3";
 
 function App() {
-  return <>
-  {/* <Bai1/> */}
-  <Bai2/>
-  </>;
+  return (
+    <>
+      {/* <Bai1/> */}
+      {/* <Bai2/> */}
+      <Bai3 />
+    </>
+  );
 }
 
 export default App;
