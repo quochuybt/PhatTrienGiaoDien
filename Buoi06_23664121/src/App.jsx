42b1bc684dcd84320d9d02b2b2196b24d9ece0c7
Huy <quochuybt2005@gmail.com>
2026-04-15 22:41:11 +0700

    feat: bai4

--- Buoi06_23664121/src/App.jsx
+++ Buoi06_23664121/src/App.jsx
@@ -3,6 +3,7 @@ import Test from "./components/area/Test";
 import Bai1 from "./components/bai1/Bai1";
 import Bai2 from "./components/bai2/Bai2";
 import Bai3 from "./components/bai3/Bai3";
+import Bai4 from "./components/bai4/Bai4";
 import Bai5 from "./components/bai5/Bai5";
 import Cart from "./components/bai5/Cart";
 import ListProduct from "./components/bai5/ListProduct";
@@ -13,8 +14,9 @@ function App() {
       {/* <Bai1 /> */}
       {/* <Bai2 /> */}
       {/* <Bai3 /> */}
+      <Bai4 />
       {/* <Test /> */}
-      <Bai5 />
+      {/* <Bai5 /> */}
     </>
   );
 }
---
+++ Buoi06_23664121/src/components/bai4/Bai4.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Bai4 = () => {
+  return <div>Bai4</div>;
+};
+
+export default Bai4;
